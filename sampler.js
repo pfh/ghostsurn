d1c99200e73059818dfc6c3072b2d7648ba7165c
Paul Harrison <pfh@logarithmic.net>
2021-10-11 12:55:22 +1100

    many things tried, some progress

--- sampler.js
+++ sampler.js
@@ -21,10 +21,18 @@ function shuffle(array) {
     }
 }
 
-function get_samples(n, choices, effort, get_check, callback) {
+/* Sample a set of words produced by making [n] choices amongst [choices].
+
+   initial is an initial blank slate.
+   
+   get_advancer returns a function to advance a word by making the ith choice. If the choice is invalid, the function will return null.
+
+   callback is periodically called back with current set of words.
+ */
+function get_samples(initial, n, choices, effort, get_advancer, callback) {
     let n_choices = choices.length;
     let saturated = null;
-    let words = [ "" ];
+    let words = [ initial ];
     
     let last_callback = Date.now();
 
@@ -32,12 +40,12 @@ function get_samples(n, choices, effort, get_check, callback) {
         let todo = Array.from(range(words.length * n_choices));
         shuffle(todo);
         
-        let check = get_check(i+1);
+        let advancer = get_advancer(i);
 
         let new_words = [ ];
         for(let j of todo) {
-            let item = words[j/n_choices>>0] + choices[j % n_choices];
-            if (!check(item)) continue;
+            let item = advancer(words[j/n_choices>>0], choices[j % n_choices]);
+            if (item === null) continue;
             new_words.push(item);
             if (new_words.length >= effort) break;
         }
@@ -47,7 +55,7 @@ function get_samples(n, choices, effort, get_check, callback) {
 
         words = new_words;
         
-        if (callback !== null && Date.now() > last_callback + 100) {
+        if (callback !== null && Date.now() > last_callback + 200) {
             callback({words, saturated});
             last_callback = Date.now();
         }
@@ -58,9 +66,10 @@ function get_samples(n, choices, effort, get_check, callback) {
 
 
 class Validity {
-    constructor(width, height, choices) {
+    constructor(width, height, initial, choices) {
         this.width = width;
         this.height = height;
+        this.initial = initial;
         this.choices = choices;
     }
 
@@ -68,25 +77,36 @@ class Validity {
         if (callback === null) 
             callback = ()=>null;
         
-        let resulter = (result, finished) => ({ 
-            ...this,
-            ...result, 
-            effort:effort, 
-            finished:finished, 
-        });
+        let resulter = (result, finished) => {
+            let output = { 
+                ...this,
+                ...result, 
+                effort:effort, 
+                finished:finished
+            }
+            
+            //Only report one sample while running, save some time decoding.
+            if (!finished) output.words = output.words.slice(0,1);
+            
+            output.words = output.words.map(item => this.get_state_word(item));
+            return output;
+        };
         function callback2(words) { 
             callback(resulter(words, false)); 
         }
 
         let words = get_samples(
+            this.initial,
             this.width*this.height, 
             this.choices, 
             effort, 
-            this.get_check.bind(this),
+            this.get_advancer.bind(this),
             callback2);
 
         return resulter(words, true);
     }
+    
+    get_state_word(state) { return state; }
 }
 
 
@@ -98,16 +118,27 @@ class Validity_pat extends Validity {
                 choices.add(choice);
         choices = Array.from(choices);
         
-        super(width, height, choices);
+        super(width, height, "", choices);
         
         this.xs = xs;
         this.ys = ys;
         this.valids = valids;
+        
+        this.subvalid_cache = { };
+    }
+    
+    get_advancer(i) {
+        let check = this.get_check(i);
+        
+        return (word, choice) => {
+            let new_word = word + choice;
+            if (!check(new_word)) return null;
+            return new_word;
+        }
     }
     
     // Returns a function to check words of a given length
-    get_check(word_length) {
-        let i = word_length-1;
+    get_check(i) {
         let x_last = i % this.width;
         let y_last = (i / this.width)>>0;
         let checks = [ ];
@@ -127,14 +158,42 @@ class Validity_pat extends Validity {
             
             if (valid_index.length <= 1) 
                 continue;
-            
-            let subvalids = new Set();
-            for(let valid of this.valids) {
-                subvalids.add( valid_index.map(j => valid[j]).join("") );
+
+            let key = JSON.stringify(valid_index);
+            if (!this.subvalid_cache.hasOwnProperty(key)) {
+                let subvalids = new Set();
+                for(let valid of this.valids) {
+                    subvalids.add( valid_index.map(j => valid[j]).join("") );
+                }
+                this.subvalid_cache[key] = subvalids;
             }
             
-            checks.push({word_index, subvalids});
+            checks.push({word_index, subvalids:this.subvalid_cache[key]});
         }
+        
+        //checks.sort( (a,b) => b.length-a.length );
+        //
+        //// Remove any checks entirely contained in another check.
+        //// ** Assumes valids have been pruned. **
+        //let c=0;
+        //console.log([checks.length]);
+        //while(c<checks.length) {
+        //    let subsumed = false;
+        //    outer: for(let j=0;j<c;j++) {
+        //        for(let k=0;k<checks[c].word_index.length;k++)
+        //        if (checks[j].word_index.indexOf( checks[c].word_index[k] ) == -1)
+        //               continue outer;
+        //        
+        //        subsumed = true;
+        //        continue
+        //    }
+        //    
+        //    if (subsumed) {
+        //        checks.splice(c,1);
+        //    } else
+        //        c++;
+        //}
+        //console.log([checks.length, "to"]);
         
         return (word) => {
             for(let check of checks) {
@@ -148,6 +207,143 @@ class Validity_pat extends Validity {
     }
 }
 
+// WaveFunctionCollapse-like validity checking
+// Note: Unlike WFC, this makes choices in raster scan order.
+class Validity_WFC extends Validity {
+    constructor(width, height, xs, ys, valids) {
+        let choices = new Set();
+        for(let word of valids)
+            for(let choice of word)
+                choices.add(choice);
+        choices = Array.from(choices);
+        
+        super(width, height, ["", new Uint8Array(width*height*choices.length).fill(1)], choices);
+        
+        this.choices = choices;
+        this.choice_map = { };
+        for(let i of range(choices.length))
+            this.choice_map[choices[i]] = i;
+        
+        this.width = width;
+        this.height = height;
+        this.n = this.width*this.height;
+        this.nc = choices.length;
+        
+        this.xs = xs;
+        this.ys = ys;
+        this.valids = valids.map(word => 
+            Array.from(word).map(c => 
+                this.choice_map[c]));
+    }
+    
+    get_state_word(state) { return state[0]; }
+    
+    get_advancer(i) {
+        return (state, choice) => {
+            let [word, on] = state;
+            on = new Uint8Array(on);
+            
+            if (!this.set(on, word.length, this.choice_map[choice]))
+                return null;
+            
+            return [word+choice, on];
+        }
+    }
+    
+    set(on, i, c) {
+        for(let j=0;j<this.nc;j++)
+        if (j != c)
+            on[i*this.nc+j] = false;
+        
+        return this.propagate_checks(on, i);
+    }
+    
+    propagate_checks(on, i) {
+        let xs=this.xs, ys=this.ys, width=this.width, height=this.height, nc=this.nc;
+        
+        let todo = new Set();
+        //todo.add(i);
+        let xi = i % width;
+        let yi = (i / width)>>0;
+        
+        for(let cind=0;cind<xs.length;cind++)
+        for(let ind=0;ind<xs.length;ind++) {
+            let x = xi + xs[ind] - xs[cind];
+            let y = yi + ys[ind] - ys[cind];
+            if (x < 0 || y < 0 || x >= width || y >= height)
+                continue;
+            if (y*width+x >= i) //Assumes raster scan choices.
+                todo.add(y*width+x);
+        }            
+        
+        
+        while(todo.size) {
+            let i = Math.min(...todo);
+            todo.delete(i);
+            
+            let [any_good, any_changed] = this.check(on, i);
+            
+            if (!any_good)
+                return false;
+            
+            if (!any_changed)
+                continue;
+            
+            let xi = i % width;
+            let yi = (i / width)>>0;
+            
+            for(let cind=0;cind<xs.length;cind++)
+            for(let ind=0;ind<xs.length;ind++) {
+                let x = xi + xs[ind] - xs[cind];
+                let y = yi + ys[ind] - ys[cind];
+                if (x < 0 || y < 0 || x >= width || y >= height)
+                    continue;
+                if (y*width+x >= i) //Assumes raster scan choices
+                    todo.add(y*width+x);
+            }            
+        }
+        
+        return true;
+    }
+    
+    // Update validity of each choice at position i in on
+    check(on, i) {
+        let nc=this.nc, n=this.n, xs=this.xs, ys=this.ys, width=this.width, height=this.height;
+        let xi = i % width;
+        let yi = (i / width)>>0;
+        let any_good = false, any_changed = false;
+        
+        check_choices: for(let j=0;j<nc;j++) {
+            if (!on[i*nc+j]) continue;
+        
+            check_centers: for(let cind=0;cind<xs.length;cind++) {
+                check_valids: for(let valid of this.valids) {
+                    for(let ind=0;ind<xs.length;ind++) {
+                        let x = xi + xs[ind] - xs[cind];
+                        let y = yi + ys[ind] - ys[cind];
+                        if (x < 0 || y < 0 || x >= width || y >= height)
+                            continue;
+                        if (!on[(y*width+x)*nc+valid[ind]])
+                            continue check_valids;
+                    }
+                    
+                    // A valid matched, continue
+                    continue check_centers;
+                }
+                
+                // No valids matched, ban choice
+                on[i*nc+j] = 0;
+                any_changed = true;
+                continue check_choices;
+            }
+            
+            // The choice was not banned
+            any_good = true;
+        }
+        
+        return [any_good, any_changed];
+    }
+}
 
 //function expand(max_width, max_height, xs,ys,words, max_elaboration) {
 //    function saturation_abort(status) {
@@ -194,11 +390,247 @@ class Validity_pat extends Validity {
 //}
 
 
+
+// Tile elaborate: for xs,ys,valids,  tilex,tiley
+// new choices become choices^tail.length
+// enumerate all valids^tile.length, check self-consistency
+
+function thick_tiles(xo, yo, xs, ys, valids) {
+    let choices = new Set();
+    for(let word of valids)
+        for(let choice of word)
+            choices.add(choice);
+    choices = Array.from(choices);
+    
+    let choice_map = { };
+    for(let i of range(choices.length))
+        choice_map[choices[i]] = i;
+    
+    let nc = choices.length;
+    let nv = xs.length;
+    
+    let must_match = [ ];
+    
+    for(let i of range(nv))
+    for(let j of range(nv))
+    if (xs[i] == xs[j]+xo && ys[i] == ys[j]+yo)
+        must_match.push( [i,j] );
+    
+    let new_valids = [ ];
+    for(let valid1 of valids) {
+        outer: for(let valid2 of valids) {
+            for(let [i,j] of must_match)
+                if (valid1[i] != valid2[j])
+                    continue outer;
+            
+            let new_valid = "";
+            for(let i of range(nv))
+                new_valid += String.fromCodePoint( choice_map[valid1[i]]*nc+choice_map[valid2[i]] );
+            new_valids.push(new_valid);
+        }
+    }
+    
+    let demap = { };
+    for(let i of range(nc))
+    for(let j of range(nc))
+        demap[ String.fromCodePoint(i*nc+j) ] = choices[i]+choices[j];
+    
+    return { valids:new_valids, demap }
+}
+
+class Validity_wider extends Validity {
+    constructor(make_sub, width, height, xs, ys, valids) {
+        let thick = thick_tiles(1,0, xs,ys,valids);
+        let sub = make_sub(width-1, height, xs, ys, thick.valids);
+    
+        super(width,height,sub.initial,sub.choices);
+        
+        this.sub = sub;
+        this.demap = thick.demap;
+    }
+    
+    get_state_word(state) {
+        state = this.sub.get_state_word(state);
+        
+        let result = "";
+        let i=0;
+        outer: for(let y of range(this.height))
+            for(let x of range(this.width-1)) {
+                if (i >= state.length) break outer;
+                if (x == this.width-2)
+                    result += this.demap[state[i]];
+                else
+                    result += this.demap[state[i]][0];
+                i++;
+            }
+        
+        return result;
+    }
+    
+    get_advancer(i) { return this.sub.get_advancer(i); }
+}
+
+class Validity_higher extends Validity {
+    constructor(make_sub, width, height, xs, ys, valids) {
+        let thick = thick_tiles(0,1, xs,ys,valids);
+        let sub = make_sub(width, height-1, xs, ys, thick.valids);
+    
+        super(width,height,sub.initial,sub.choices);
+        
+        this.sub = sub;
+        this.demap = thick.demap;
+    }
+    
+    get_state_word(state) {
+        state = this.sub.get_state_word(state);
+
+        let result = "", result_extra = "";
+        let i=0;
+        outer: for(let y of range(this.height-1))
+            for(let x of range(this.width)) {
+                if (i >= state.length) break outer;
+                result += this.demap[state[i]][0];
+                if (y == this.height-2)
+                    result_extra += this.demap[state[i]][1];
+                i++;
+            }
+        
+        return result + result_extra;
+    }
+    
+    get_advancer(i) { return this.sub.get_advancer(i); }
+}
+
+// complete failure
+function make_stack(thick_width, thick_height, width, height, xs, ys, valids) {
+    console.log( valids.length );
+    if (thick_width <= 0 && thick_height <= 0) {
+        let result = new Validity_WFC(width, height, xs, ys, valids);
+        console.log(result.choices.length + " choices");
+        return result;
+    }
+    
+    if (thick_width > thick_height)
+        return new Validity_wider( (...p) => make_stack(thick_width-1, thick_height, ...p), width, height, xs, ys, valids )
+    
+    return new Validity_higher( (...p) => make_stack(thick_width, thick_height-1, ...p), width, height, xs, ys, valids )
+}
+
+
+function elaborate(x1, y1, xs, ys, valids) {    
+    let new_xs = xs.slice();
+    let new_ys = ys.slice();
+    let new_valids = [ ];
+    let grab1 = [ ];
+    let must_match0 = [ ];
+    let must_match1 = [ ];
+
+    outer: for(let i of range(xs.length)) {
+        for(let j of range(xs.length))
+        if (xs[i]+x1 == xs[j] && ys[i]+y1 == ys[j]) {
+            must_match0.push(j);
+            must_match1.push(i);
+            continue outer;
+        }
+        grab1.push(i);
+        new_xs.push(xs[i]+x1);
+        new_ys.push(ys[i]+y1);
+    }
+    
+    let subword_valid1 = { };
+    for(let valid1 of valids) {
+       let subword = must_match1.map(i => valid1[i]).join("");
+       if (!subword_valid1.hasOwnProperty(subword))
+          subword_valid1[subword] = [ ];
+        subword_valid1[subword].push(valid1);
+    }
+    
+    for(let valid0 of valids) {
+        let subword = must_match0.map(i => valid0[i]).join("");
+        if (!subword_valid1.hasOwnProperty(subword))
+            continue;
+        for(let valid1 of subword_valid1[subword]) {
+            new_valids.push(valid0 + grab1.map(i => valid1[i]).join(""));
+        }
+    }
+    
+    return {xs:new_xs, ys:new_ys, valids:new_valids};
+}
+
+
+function prune(xs, ys, valids) {
+    let all_overlaps = [ ];
+
+    for(let i of range(xs.length))
+    for(let j of range(xs.length))
+    if (i != j) {
+        let overlap = [ xs[j]-xs[i], ys[j]-ys[i] ];
+        if (all_overlaps.filter(item=> item[0]==overlap[0] && item[1]==overlap[1]).length) continue;
+        all_overlaps.push(overlap);
+    }
+
+    while(true) { 
+        let any = false;
+        for(let [x1,y1] of all_overlaps) {
+
+            let new_valids = [ ];
+            let must_match0 = [ ];
+            let must_match1 = [ ];
+            outer: for(let i of range(xs.length)) {
+                for(let j of range(xs.length))
+                if (xs[i]+x1 == xs[j] && ys[i]+y1 == ys[j]) {
+                    must_match0.push(j);
+                    must_match1.push(i);
+                    continue outer;
+                }
+            }
+            
+            let good_set = new Set(valids.map(valid => must_match1.map(i => valid[i]).join("")));
+        
+            for(let valid of valids) {
+                let subword = must_match0.map(i => valid[i]).join("");
+                if (good_set.has(subword))
+                    new_valids.push(valid);
+            }
+            
+            any = any || (valids.length > new_valids.length);
+            valids = new_valids;
+        }
+        
+        if (!any) break;
+    }
+        
+    return valids;
+}
+
 function run_job(width, height, xs, ys, words, effort) {
     //let best = expand(width, height, xs,ys,words, 100000);
     //console.log(best);
 
-    let validity = new Validity_pat(width, height, xs,ys,words);
+    //let validity = new Validity_pat(width, height, xs,ys,words);
+    //let validity = new Validity_WFC(width, height, xs,ys,words);
+    //let validity = make_stack(Math.min(width, 0), Math.min(height, 0), width, height, xs,ys,words);
+    let spec={xs:xs,ys:ys,valids:words};
+    spec.valids = prune(spec.xs, spec.ys, spec.valids);
+
+    let n_stop = 5000;
+    let seq = "";
+    for(let i of range(3)) {
+        if (spec.valids.length >= n_stop) break
+        seq += "w";
+        spec = elaborate(1,0, spec.xs, spec.ys, spec.valids);
+        spec.valids = prune(spec.xs, spec.ys, spec.valids);
+        
+        if (spec.valids.length >= n_stop) break
+        seq += "h";
+        spec = elaborate(0,1, spec.xs, spec.ys, spec.valids);
+        spec.valids = prune(spec.xs, spec.ys, spec.valids);
+    }
+    
+    console.log([ seq, spec.xs.length, spec.valids.length ]);
+    
+    let validity = new Validity_pat(width, height, spec.xs,spec.ys,spec.valids);
+
     let result = validity.sample(effort, result => self.postMessage(result));        
     self.postMessage(result);
 }
--- test.js
+++ test.js
@@ -3,13 +3,30 @@ let fs = require("fs");
 let vm = require("vm");
 vm.runInThisContext(fs.readFileSync("sampler.js"))
 
-let val = new Validity_pat(80,20, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
+if (false) {
+    let result = thick_tiles(0,1, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
 
-let result = val.sample(10, null);
-
-for(word of result.words) {
-    for(y of range(val.height)) 
-        console.log(word.slice(y*val.width,(y+1)*val.width));
-    console.log("");
+    console.log(result);
 }
 
+if (true) {
+    //let val = new Validity_pat(80,20, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
+
+    //let val = new Validity_WFC(20,10, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
+    //let val = new Validity_wider((...p) => new Validity_pat(...p), 20,10, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
+    //let val = make_stack(2,2,  20,10, [1,0,1], [0,1,1], ["   "," ##","# #","## "]);
+    
+    let spec = {xs:[1,0,1],ys:[0,1,1],valids:["   "," ##","# #","## "]};
+    spec = elaborate(1,0, spec.xs, spec.ys, spec.valids);
+    spec = elaborate(0,1, spec.xs, spec.ys, spec.valids);
+    console.log(spec);
+    let val = new Validity_pat(20,10, spec.xs,spec.ys,spec.valids);
+
+    let result = val.sample(10, null);
+
+    for(word of result.words) {
+        for(y of range(val.height)) 
+            console.log(word.slice(y*val.width,(y+1)*val.width));
+        console.log("");
+    }
+}
